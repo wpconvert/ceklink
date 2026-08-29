167eb74be82273cbe5ac63801e07d0b9dc2c79e9
convert <danip4848@gmail.com>
2026-08-30 04:35:13 +0700

    script.js

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxIfEy-VaYDV4Y6AxzdbfdJE8wVX_lOdpxYX4D4S5qrdEvCKXRZHlRsFYRxei2tH5OL/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzgsu-jPD_6v6K2TxuH6qbvRsiNyDyWKDdtTvKMRQ_kyOUYLpSB_njOpBiJ_-h9pnwK/exec";
 
 let links = [];
 
